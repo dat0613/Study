91c0d9b733d836d3ac24cef98bf33f3603b2148e
dat0613 <dat0613@naver.com>
2022-12-24 22:25:43 +0900

    docker stop 커맨드 테스트

--- CMake/Tutorial/src/executable-example/main.cpp
+++ CMake/Tutorial/src/executable-example/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include "static.hpp"
+#include <signal.h>
+#include <thread>
+
+volatile bool stop = false;
+
+void CallbackSIGTERM(int signal)
+{
+	stop = true;
+}
 
 int main()
 {
+	signal(SIGTERM, &CallbackSIGTERM);
+
 	std::cout << "Hello exe!" << std::endl;
 
 	StaticLibrary lib;
-	lib.DoSomeThing();
+
+	while (false == stop)
+	{
+		lib.DoSomeThing();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+
+	if (stop)
+	{
+		std::cout << "Signal SIGTERM was received." << std::endl;
+		std::cout << "This container will stop in 5 seconds." << std::endl;
+	}
+
+	std::this_thread::sleep_for(std::chrono::seconds(5));
 }
